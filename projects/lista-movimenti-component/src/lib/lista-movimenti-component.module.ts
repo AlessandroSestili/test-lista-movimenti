a5698fe32cfa1bc62b3745eecfa9458f82530a6d
lucaburale <luca.burale@S2E-mail.com>
2022-05-02 11:44:17 +0200

    install fxLayout e Material

--- projects/lista-movimenti-component/src/lib/lista-movimenti-component.module.ts
+++ projects/lista-movimenti-component/src/lib/lista-movimenti-component.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { ListaMovimentiComponentComponent } from './lista-movimenti-component.component';
 
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
 
 
 @NgModule({
@@ -8,6 +13,10 @@ import { ListaMovimentiComponentComponent } from './lista-movimenti-component.co
     ListaMovimentiComponentComponent
   ],
   imports: [
+    FlexLayoutModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule
   ],
   exports: [
     ListaMovimentiComponentComponent
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ListaMovimentiComponentModule } from 'projects/lista-movimenti-component/src/public-api';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    ListaMovimentiComponentModule
+    ListaMovimentiComponentModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
